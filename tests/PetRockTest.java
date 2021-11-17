c7ec0d8405ac590fb0fa6f6ccb40720902427bcd
ismetisik <34589935+ismetisik@users.noreply.github.com>
2021-11-17 15:54:41 -0600

    Add files via upload

---
+++ tests/PetRockTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PetRockTest {
+
+    @Test
+    public void getName() throws Exception {
+        PetRock rocky = new PetRock("Rocky");
+        assertEquals("Rocky", rocky.getName());
+
+    }
+
+    @Test
+    public void name() throws Exception{
+        PetRock rocky = new PetRock("Rocky");
+        assertFalse(rocky.isHappy());
+    }
+}
